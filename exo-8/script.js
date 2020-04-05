a6524b10058cdc6005f62170e9f1e54ea4b6ea51
florent69 <46051667+florent69@users.noreply.github.com>
2020-04-05 16:44:05 +0200

    Add files via upload
    plugin itemSlide

--- exo-8/script.js
+++ exo-8/script.js
@@ -1,1 +1,12 @@
 console.log("exercice 8");
+
+var carousel;
+$(document).ready(function () {
+    carousel = $("ul");
+    carousel.itemslide(
+        {
+            disable_slide: false,
+            duration: 150
+        }
+    );
+});
